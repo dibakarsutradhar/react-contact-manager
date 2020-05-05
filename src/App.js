fe9514d6a33275f4ba0a47f7ba9db3f730be8208
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-05-05 20:48:56 +0800

    Bootstrap setuped

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header branding="Contact Manager" />
-      <Contact name="John Doe" email="jdoe@gmail.com" phone="100-000-0000" />
-      <Contact name="Karen Doe" email="kdoe@gmail.com" phone="100-000-0000" />
+      <div className="container">
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="100-000-0000" />
+        <Contact name="Karen Doe" email="kdoe@gmail.com" phone="100-000-0000" />
+      </div>
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,11 +5,11 @@ class Contact extends Component {
   render() {
     const { name, email, phone } = this.props;
     return (
-      <div>
+      <div className="card card-body mb-3">
         <h4>{name}</h4>
-        <ul>
-          <li>{email}</li>
-          <li>{phone}</li>
+        <ul className="list-group">
+          <li className="list-group-item">{email}</li>
+          <li className="list-group-item">{phone}</li>
         </ul>
       </div>
     );
