a24ebae2d4d8fde412a36f17c8e734ebbe15351f
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-05-06 22:04:35 +0800

    Cleaned src folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Contacts from "./components/Contacts";
-import Header from "./components/Header";
+import Contacts from "./components/contacts/Contacts";
+import Header from "./components/layout/Header";
+import AddContact from "./components/contacts/AddContact";
 
 import { Provider } from "./context";
 
@@ -13,6 +14,7 @@ function App() {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
+          <AddContact />
           <Contacts />
         </div>
       </div>
