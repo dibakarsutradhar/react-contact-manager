6d5d5c54fcb056564ef6bc7c9e8ddbc8b13fd0c1
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-05-08 13:35:38 +0800

    Updates

---
+++ precache-manifest.143323233b1669256cce2e0104b9321d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740855534ddad075c75ed52a9cbf1803",
+    "url": "/react-contact-manager/index.html"
+  },
+  {
+    "revision": "ecc43e8ee61d8fd34c6e",
+    "url": "/react-contact-manager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "8df199e2323b0bb0ea65",
+    "url": "/react-contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ecc43e8ee61d8fd34c6e",
+    "url": "/react-contact-manager/static/js/2.972ce7cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-contact-manager/static/js/2.972ce7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df199e2323b0bb0ea65",
+    "url": "/react-contact-manager/static/js/main.9d97dda5.chunk.js"
+  },
+  {
+    "revision": "f91e1ae1b7987184080d",
+    "url": "/react-contact-manager/static/js/runtime-main.a36a380e.js"
+  }
+]);
