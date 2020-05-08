eb6db2d66bcc025d68dd4a8ae17387e7f515ead6
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2020-05-08 13:32:55 +0800

    BrowserRouter to HashRouter converstion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Contacts from "./components/contacts/Contacts";
 import Header from "./components/layout/Header";
 import AddContact from "./components/contacts/AddContact";
